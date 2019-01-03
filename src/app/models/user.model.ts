8d259f04cc5db13e2325f5a3fe0ccfce9daae899
unknown <yury_sakovich@smartexlab.com>
2019-01-03 13:31:14 +0300

    setup | setup prettier

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,8 +1,8 @@
 export interface User {
-    id?: string,
-    name?: string,
-    email?: string,
-    password?: string,
-    token: string;
-    expiration: number;
+  id?: string;
+  name?: string;
+  email?: string;
+  password?: string;
+  token: string;
+  expiration: number;
 }
