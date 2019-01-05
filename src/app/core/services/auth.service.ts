ba290e729e2777da280e27611a3d94de311460ca
kanonirbrest <yury_sakovich@smartexlab.com>
2019-01-05 12:10:58 +0300

    fix style issue

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { environment } from '@env';
+import {Injectable} from '@angular/core';
+import {Router} from '@angular/router';
+import {environment} from '@env';
 
-import { Observable } from 'rxjs';
+import {Observable} from 'rxjs';
 
-import { LocalStorageService } from './local-storage.service';
-import { ApiService } from './api.service';
+import {LocalStorageService} from './local-storage.service';
+import {ApiService} from './api.service';
 
 @Injectable()
 export class AuthService {
@@ -13,14 +13,25 @@ export class AuthService {
   private AUTH_URL = environment.api.authUrl;
   private LOCAL_URL = environment.api.localUrl;
 
-  constructor(private localStorageService: LocalStorageService,
-              private apiService: ApiService,
-              private router: Router) {}
+  constructor(
+    private localStorageService: LocalStorageService,
+    private apiService: ApiService,
+    private router: Router
+  ) {}
 
   public signIn(credentials: any): Observable<any> {
     const body = credentials;
+    this.team(141).subscribe((data) => {
+      console.log(data, 'data - team');
+    });
 
     return this.apiService.post(`${this.AUTH_URL}/auth/login`, {body});
+  }
+
+  // /api/v11/portal/team/payee/{payeeId}
+
+  public team(payeeId: any): Observable<any> {
+    return this.apiService.get(`${this.API_URL}/portal/team/payee/${141}`);
   }
 
   public logOut(): void {
