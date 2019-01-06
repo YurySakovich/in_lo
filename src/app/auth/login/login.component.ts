4abcd285439f724c804a5cf576efcb99eab462a4
yury.sakovich <kanonirw@gmail.com>
2019-01-06 16:15:17 +0300

    change fake data for login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,13 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { emailValidator } from '@shared/constants/validators';
-import { Store } from '@ngrx/store';
-import { User } from '../../models';
-import {
-  RootStoreState,
-  AuthFeatureStoreActions
-} from '../../root-store';
-import { Observable } from 'rxjs';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+import {emailValidator} from '@shared/constants/validators';
+import {Store} from '@ngrx/store';
+import {User} from '../../models';
+import {RootStoreState, AuthFeatureStoreActions} from '../../root-store';
+import {Observable} from 'rxjs';
 
 export interface AuthData {
   token: string;
@@ -17,16 +14,14 @@ export interface AuthData {
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
   public loginForm: FormGroup;
   public buildInfo;
   AuthFeatureItems$: Observable<User[]>;
 
-  constructor(
-    private store$: Store<RootStoreState.State>,
-    private fb: FormBuilder) { }
+  constructor(private store$: Store<RootStoreState.State>, private fb: FormBuilder) {}
 
   ngOnInit() {
     this.loginForm = this.initForm();
@@ -35,15 +30,12 @@ export class LoginComponent implements OnInit {
   private initForm(): FormGroup {
     return this.fb.group({
       client: ['demo01', Validators.required],
-      username: ['demo.user@inlogic.ca', [Validators.required, Validators.pattern(emailValidator)]],
-      password: ['thedemo01', Validators.required]
+      username: ['email1@test.com', [Validators.required, Validators.pattern(emailValidator)]],
+      password: ['theemail1', Validators.required],
     });
   }
 
   public onSubmit(): void {
-    this.store$.dispatch(
-      new AuthFeatureStoreActions.LoginRequestAction(this.loginForm.value)
-    );
+    this.store$.dispatch(new AuthFeatureStoreActions.LoginRequestAction(this.loginForm.value));
   }
-
 }
